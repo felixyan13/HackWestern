08616fdf693bd28389db9c8b7f0563c7f076034d
Elton Kok <elton.kok@mail.utoronto.ca>
2020-11-21 15:11:59 -0500

    Initial set up

---
+++ shopping_extension/scripts/popup.js
@@ -0,0 +1,15 @@
+let changeColour = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", (data) => {
+    changeColour.style.backgroundColor = data.color;
+    changeColour.setAttribute('value', data.color);
+})
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+};
